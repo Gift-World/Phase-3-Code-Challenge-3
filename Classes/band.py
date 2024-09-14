5c4a13473edf7b97fbda97ab75e66ce1544c9b6c
charles <charles.angila@student.moringaschool.com>
2024-09-14 21:33:06 +0300

    "Removed __repr__ method from Band class"

--- Classes/band.py
+++ Classes/band.py
@@ -5,5 +5,5 @@ class Band:
         self.name = name
         self.hometown = hometown
         
-    def __repr__(self) :
-        return f"Band({self.id}:, {self.name}:, {self.hometown})"    
+        
+          
